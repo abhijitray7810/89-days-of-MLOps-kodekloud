23a2b5daf4ab457ef456e92abc48c174496bab68
Abhijit RAY <royabhijits928@gmail.com>
2026-05-12 22:58:11 +0530

    Create jupyter_lab_config.py

---
+++ Day 2: Set Up and Configure Jupyter Notebook Server/jupyter_lab_config.py
@@ -0,0 +1,7 @@
+c.ServerApp.token = ''
+c.ServerApp.password = ''
+c.ServerApp.disable_check_xsrf = True
+
+c.ServerApp.notebook_dir = '/root/notebooks'
+c.ServerApp.port = 8888
+c.ServerApp.ip = '0.0.0.0'
